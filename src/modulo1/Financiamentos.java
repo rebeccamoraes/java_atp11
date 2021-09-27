a14ea6d61227b44196043d34284851fe50876dfe
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-09-27 12:28:15 -0300

    Adicionando exibicao do menu.

--- src/modulo1/Financiamentos.java
+++ src/modulo1/Financiamentos.java
@@ -3,5 +3,13 @@ package modulo1;
 public class Financiamentos {
     public static void main(String[] args) {
         System.out.println("============= Bradesco Financiamentos =============");
+
+        //menu Crédito Pessoal, Crédito Imobiliário, Crédito Empresarial e Empréstimo Consignado.
+        System.out.println("Menu");
+        System.out.println("1. Crédito Pessoal");
+        System.out.println("2. Crédito Imobiliário");
+        System.out.println("3. Crédito Empresarial");
+        System.out.println("4. Crédito Consignado");
+        System.out.println("0. Sair");
     }
 }
