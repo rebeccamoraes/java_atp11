8dd4d9738236f1e0149db056f9e954edf7427518
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-09-27 13:23:12 -0300

    Adicionando validação ao menu.

--- src/modulo1/Financiamentos.java
+++ src/modulo1/Financiamentos.java
@@ -1,15 +1,56 @@
 package modulo1;
 
+import java.util.Scanner;
+
 public class Financiamentos {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        short opcao = 0;
+        boolean continua = false;
+
+
         System.out.println("============= Bradesco Financiamentos =============");
 
-        //menu Crédito Pessoal, Crédito Imobiliário, Crédito Empresarial e Empréstimo Consignado.
-        System.out.println("Menu");
-        System.out.println("1. Crédito Pessoal");
-        System.out.println("2. Crédito Imobiliário");
-        System.out.println("3. Crédito Empresarial");
-        System.out.println("4. Crédito Consignado");
-        System.out.println("0. Sair");
+        do {
+            System.out.println("Menu");
+            System.out.println("1. Crédito Pessoal");
+            System.out.println("2. Crédito Imobiliário");
+            System.out.println("3. Crédito Empresarial");
+            System.out.println("4. Crédito Consignado");
+            System.out.println("0. Sair");
+            
+            System.out.print("Digite o número correspondente a opção desejada: ");
+            opcao = Short.parseShort(scanner.nextLine());
+
+            continua = false;
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("========= Crédito Pessoal ==========");
+                    break;
+
+                case 2:
+                    System.out.println("========= Crédito Imobiliário =========");
+                    break;
+                
+                case 3:
+                    System.out.println("========= Crédito Empresarial =========");
+                    break;
+                
+                case 4:
+                    System.out.println("========= Crédito Consignado =========");
+                    break;
+
+                case 5:
+                    System.out.println("========= Aplicação finalizada =========");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+                    continua = true;
+                    break;
+            }
+
+        } while(continua);
     }
 }
