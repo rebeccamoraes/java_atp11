09a1f270ff4897cfb9860bc088dc9bddd9eff156
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-09-27 14:20:50 -0300

    Passando validacao para o metodo menu.

--- src/modulo1/Financiamentos.java
+++ src/modulo1/Financiamentos.java
@@ -36,22 +36,15 @@ public class Financiamentos {
                     System.out.println("========= Aplicação finalizada =========");
                     continua = false;
                     break;
-
-                default:
-                    System.out.println("Opção inválida.");
-                    continua = true;
-                    break;
             }
 
             char resposta = '0';
 
-            while ((opcao > 0 && opcao < 5) && (resposta != 'S' && resposta != 'V')) {
-                System.out.println("Digite 'V' para voltar ao menu principal ou 'S' para sair: ");
+            while ((opcao != 0) && (resposta != 'S' && resposta != 'V')) {
+                System.out.print("Digite 'V' para voltar ao menu principal ou 'S' para sair: ");
                 resposta = scanner.nextLine().toUpperCase().charAt(0);
 
-                if (resposta == 'V') {
-                    continua = true;
-                }
+                continua = resposta == 'V';
             }
 
         } while(continua);
@@ -61,17 +54,24 @@ public class Financiamentos {
 
     static short menu () {
         Scanner scanner = new Scanner(System.in);
+        boolean opcaoIvalida = false;
+        short opcao;
 
-        System.out.println("Menu");
-        System.out.println("1. Crédito Pessoal");
-        System.out.println("2. Crédito Imobiliário");
-        System.out.println("3. Crédito Empresarial");
-        System.out.println("4. Crédito Consignado");
-        System.out.println("0. Sair");
+        do {
+            System.out.println("Menu");
+            System.out.println("1. Crédito Pessoal");
+            System.out.println("2. Crédito Imobiliário");
+            System.out.println("3. Crédito Empresarial");
+            System.out.println("4. Crédito Consignado");
+            System.out.println("0. Sair");
+            
+            System.out.print("Digite o número correspondente a opção desejada: ");
+            opcao = Short.parseShort(scanner.nextLine());
+
+            opcaoIvalida = opcao < 0 || opcao > 4;
+        } while (opcaoIvalida);
         
-        System.out.print("Digite o número correspondente a opção desejada: ");
-        short opcao = Short.parseShort(scanner.nextLine());
-
+        //scanner.close();
         return opcao;
     }
 }
